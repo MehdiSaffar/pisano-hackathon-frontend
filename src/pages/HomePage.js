902aa4ca86b7a5089ac39f5531fe75d0f495fe11
Mehdi Saffar <saffar.mehdi3@gmail.com>
2018-10-13 16:45:52 +0300

    Mobx

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,12 @@ import { observer, inject } from "mobx-react"
 import { observable } from "mobx"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import classes from "./HomePage.css"
+import ReactAutocomplete from "react-autocomplete"
+const mockItems = [
+    { id: "passport", label: "Passport" },
+    { id: "student_document", label: "Student document" },
+    { id: "birth_certificate", label: "Birth certificate" },
+]
 @inject("store")
 @observer
 class HomePage extends Component {
@@ -11,14 +17,15 @@ class HomePage extends Component {
     searchText = ""
 
     onSearchTextChanged = event => {
+        console.log(event)
         this.searchText = event.target.value
     }
 
-    onSearchSubmit = event => {
-      event.preventDefault()
-      console.log("Searching for " + this.searchText)
-    }
 
+    onSearchSubmit = event => {
+        event.preventDefault()
+        console.log("Searching for " + this.searchText)
+    }
 
     render() {
         return (
@@ -26,16 +33,46 @@ class HomePage extends Component {
                 <h1 className={classes.Title}>
                     What document would you like to obtain?
                 </h1>
-                <form className={classes.SearchForm} onSubmit={this.onSearchSubmit}>
-                  <input
-                      className={classes.SearchBar}
-                      value={this.searchText}
-                      onChange={this.onSearchTextChanged}
-                      placeholder="Document name"
-                  />
-                  <button className={classes.SearchButton}>
-                    <FontAwesomeIcon icon="search" />
-                  </button>
+                <form
+                    className={classes.SearchForm}
+                    onSubmit={this.onSearchSubmit}
+                >
+                    <ReactAutocomplete
+                        items={mockItems}
+                        shouldItemRender={(item, value) =>
+                            item.label
+                                .toLowerCase()
+                                .indexOf(value.toLowerCase()) > -1
+                        }
+                        getItemValue={item => item.label}
+                        renderItem={(item, highlighted) => (
+                            <div
+                                key={item.id}
+                                className={[
+                                    classes.SearchBarItem
+                                ]}
+                            >
+                                {item.label}
+                            </div>
+                        )}
+                        inputProps={{className: classes.SearchBar}}
+                        // renderInput={props => {
+
+                        //   console.log(props)
+                        //   return <input
+                        //         className={classes.SearchBar}
+                        //         value={props.value}
+                        //         onChange={props.onChange}
+                        //         placeholder="Document name"
+                        //     />
+                        // }}
+                        value={this.searchText}
+                        onChange={this.onSearchTextChanged}
+                        onSelect={(itemName) => this.searchText = itemName}
+                    />
+                    <button className={classes.SearchButton}>
+                        <FontAwesomeIcon icon="search" />
+                    </button>
                 </form>
             </div>
         )
