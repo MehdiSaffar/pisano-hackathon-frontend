3ed325625760bd8f302afc39f682a4d8242a07e0
Mehdi Saffar <saffar.mehdi3@gmail.com>
2018-10-14 13:13:04 +0300

    fixed graph, added institution

--- src/pages/DocumentPage/GraphView/GraphView.js
+++ src/pages/DocumentPage/GraphView/GraphView.js
@@ -32,66 +32,16 @@ class GraphView extends Component {
 
         // the graph configuration, you only need to pass down properties
         // that you want to override, otherwise default ones will be used
-        // const myConfig = {
-        //     nodeHighlightBehavior: true,
-        //     node: {
-        //         color: "lightgreen",
-        //         size: 120,
-        //         highlightStrokeColor: "blue",
-        //         labelProperty: "label",
-        //     },
-        //     link: {
-        //         highlightColor: "lightblue",
-        //     },
-        //     minZoom: "4",
-        //     maxZoom: "4",
-        //     height: "400px",
-        //     width: "400px",
-        // }
-        const otherconfig = {
-            ...this.getScreenSize(),
-            automaticRearrangeAfterDropNode: false,
-            collapsible: false,
-            highlightDegree: 1,
-            highlightOpacity: 1,
-            linkHighlightBehavior: false,
-            maxZoom: 8,
-            minZoom: 0.1,
-            nodeHighlightBehavior: false,
-            panAndZoom: false,
-            staticGraph: false,
-            d3: {
-                alphaTarget: 0.05,
-                gravity: -100,
-                linkLength: 100,
-                linkStrength: 1,
-            },
+        const myConfig = {
+            nodeHighlightBehavior: true,
             node: {
-                color: "#d3d3d3",
-                fontColor: "black",
-                fontSize: 8,
-                fontWeight: "normal",
-                highlightColor: "SAME",
-                highlightFontSize: 8,
-                highlightFontWeight: "normal",
-                highlightStrokeColor: "SAME",
-                highlightStrokeWidth: 1.5,
-                labelProperty: "id",
-                mouseCursor: "pointer",
-                opacity: 1,
-                renderLabel: true,
-                size: 200,
-                strokeColor: "none",
-                strokeWidth: 1.5,
-                svg: "",
-                symbolType: "circle",
+                color: "lightgreen",
+                size: 120,
+                highlightStrokeColor: "blue",
+                labelProperty: "label",
             },
             link: {
-                color: "#d3d3d3",
-                highlightColor: "#d3d3d3",
-                opacity: 1,
-                semanticStrokeWidth: false,
-                strokeWidth: 1.5,
+                highlightColor: "lightblue",
             },
         }
 
@@ -125,14 +75,13 @@ class GraphView extends Component {
         }
 
         console.log(data)
-        // setTimeout(() => document.getElementById('graph-id-graph-wrapper').classList.add('fucker'), 1000)
         return (
             <div className={classes.GraphView}>
                 {this.documentStore.documents.length > 0 && (
                     <Graph
                         id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
                         data={data}
-                        config={otherconfig}
+                        config={myConfig}
                     />
                 )}
             </div>
